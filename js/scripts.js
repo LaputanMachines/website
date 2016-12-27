6908e2c280c73c720ba7b2806917452d91a9ae52
Michael Bassili <Michaelbassili539@gmail.com>
2016-12-26 17:32:08 -0800

    fixes navbar upon viewport scroll

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,30 @@
 // (C) 2015-16 Michael Bassili
 // ========================================
 // 
+
+      // Wow Plugin
+      // new WOW().init();
+
+      // Google Analytics
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-77100076-2', 'auto');
+      ga('send', 'pageview');
+      
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 900) {
+      $('#header').addClass('headerFixed');
+    }
+    if ($(window).scrollTop() < 900) {
+      $('#header').removeClass('headerFixed');
+    }
+  });
+});
