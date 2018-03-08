277a1c6b355671ba381ca48b7869dfbbd2838986
Michael Bassili <michaelbassili539@gmail.com>
2018-03-07 20:12:54 -0800

    Adds a the collapse/expand nav bar button to the outside of the element

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-function collapseNavigationBar() {
-  if (document.getElementById("navigationBar").classList.contains("expandedNavigationBar")) {
-    document.getElementById("navigationBar").classList.remove("expandedNavigationBar");
-    document.getElementById("navigationBar").classList.add("collapsedNavigationBar");
-   }
-}
-
-function expandNavigationBar() {
+function toggleNavigationBar() {
   if (document.getElementById("navigationBar").classList.contains("collapsedNavigationBar")) {
+    // Expands the navigation bar division and navigation button
     document.getElementById("navigationBar").classList.remove("collapsedNavigationBar");
     document.getElementById("navigationBar").classList.add("expandedNavigationBar");
-  }
+    document.getElementById("collapseContainer").classList.remove("collapsedNavigationButton");
+    document.getElementById("collapseContainer").classList.add("expandedNavigationButton");
+    document.getElementById("collapseButton").innerHTML="<";  // Legacy
+    document.getElementById("collapseButton").textContent="<";
+  } else if (document.getElementById("navigationBar").classList.contains("expandedNavigationBar")) {
+    // Collapses the navigation bar and navigation button
+    document.getElementById("navigationBar").classList.remove("expandedNavigationBar");
+    document.getElementById("navigationBar").classList.add("collapsedNavigationBar");
+    document.getElementById("collapseContainer").classList.remove("expandedNavigationButton");
+    document.getElementById("collapseContainer").classList.add("collapsedNavigationButton");
+    document.getElementById("collapseButton").innerHTML=">";  // Legacy
+    document.getElementById("collapseButton").textContent=">";
+   }
 }
