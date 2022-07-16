0b5996932c44abf068239fabd455c995dc6e6890
Michael Bassili <michaelbassili539@gmail.com>
2022-07-15 22:20:11 -0700

    Switch to using the older static site

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Load in the Particles.JS project
+// into the header of the website
+
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
